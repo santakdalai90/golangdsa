ec6d5fad730c2837ce72065699efb3d7f94e1bfc
santakdalai90 <>
2021-12-30 19:55:26 +0000

    added towers of hanoi

--- src/sorting/selection_sort.go
+++ src/sorting/selection_sort.go
@@ -2,9 +2,10 @@ package sorting
 
 // SelectionSort sorts a given array using selection sort.
 // Each iteration finds the minimum and swaps it to its position
-// Loop stops when all the indices are done
+// Loop runs till the second last position. Last position would be automatically
+// done in the last iteration
 func SelectionSort(arr []int) {
-    for i:=0; i<len(arr); i++ {
+    for i:=0; i<len(arr)-1; i++ {
         minIdx := i
         // find index of the minimum
         for j:=i+1; j< len(arr); j++ {
---
+++ test/recursion_and_backtracking/towers_of_hanoi_test.go
@@ -0,0 +1,11 @@
+package recursion_and_backtracking
+
+import (
+	//"github.com/stretchr/testify/assert"
+	"golang-dsa/src/recursion_and_backtracking"
+	"testing"
+)
+
+func TestTOH(t *testing.T) {
+	recursion_and_backtracking.TOH(3, "A", "C", "B")
+}
