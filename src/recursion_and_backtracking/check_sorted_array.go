4662436759f5087a672744e646f4ed8b50607e46
santakdalai90 <>
2021-12-30 20:05:13 +0000

    added IsSorted for recursive sort check

---
+++ src/recursion_and_backtracking/check_sorted_array.go
@@ -0,0 +1,10 @@
+package recursion_and_backtracking
+
+//IsSorted checks if an input array is sorted using recursion
+func IsSorted(arr []int) bool {
+	if len(arr) <= 1 {
+		return true
+	}
+
+	return arr[0] < arr[1] && IsSorted(arr[1:])
+}
--- src/sorting/selection_sort.go
+++ src/sorting/selection_sort.go
@@ -5,17 +5,17 @@ package sorting
 // Loop runs till the second last position. Last position would be automatically
 // done in the last iteration
 func SelectionSort(arr []int) {
-    for i:=0; i<len(arr)-1; i++ {
-        minIdx := i
-        // find index of the minimum
-        for j:=i+1; j< len(arr); j++ {
-            if arr[j] < arr[minIdx] {
-                minIdx = j
-            }
-        }
-        if minIdx != i {        // avoid swap if current number is minimum
-            // swap the minimum to its position
-            arr[i], arr[minIdx] = arr[minIdx], arr[i]
-        }
-    }
+	for i := 0; i < len(arr)-1; i++ {
+		minIdx := i
+		// find index of the minimum
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
+		}
+		if minIdx != i { // avoid swap if current number is minimum
+			// swap the minimum to its position
+			arr[i], arr[minIdx] = arr[minIdx], arr[i]
+		}
+	}
 }
