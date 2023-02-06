104888bb2078ba9b6420e89ff27313c49ee6bc67
Santak Dalai <santakdalai90@live.com>
2023-02-06 12:09:49 +0000

    Added queue implementation using stacks

--- src/data_structures/stack/stack.go
+++ src/data_structures/stack/stack.go
@@ -1,51 +1,50 @@
 package stack
 
 import (
-    "errors"
-    "fmt"
+	"fmt"
 )
 
 type Stack struct {
-    Data []interface{}    // top towards the end
+	Data []interface{} // top towards the end
 }
 
 func New() *Stack {
-    s := Stack {}
+	s := Stack{}
 
-    s.Data = make([]interface{}, 0)
+	s.Data = make([]interface{}, 0)
 
-    return &s
+	return &s
 }
 
 func (s *Stack) Push(item interface{}) error {
-    s.Data = append(s.Data, item)
-    return nil
-} 
+	s.Data = append(s.Data, item)
+	return nil
+}
 
 func (s *Stack) Pop() (interface{}, error) {
-    if s == nil || len(s.Data) == 0 {
-        return nil, errors.New("empty stack, cannot pop")
-    }
-    top := s.Data[len(s.Data) - 1]
-    s.Data = s.Data[:len(s.Data) - 1]
-    return top, nil
+	if s == nil || len(s.Data) == 0 {
+		return nil, NewStackEmptyError()
+	}
+	top := s.Data[len(s.Data)-1]
+	s.Data = s.Data[:len(s.Data)-1]
+	return top, nil
 }
 
 func (s *Stack) Top() (interface{}, error) {
-    if s == nil || len(s.Data) == 0 {
-        return nil, errors.New("empty stack")
-    }
-    top := s.Data[len(s.Data) - 1]
-    return top, nil
+	if s == nil || len(s.Data) == 0 {
+		return nil, NewStackEmptyError()
+	}
+	top := s.Data[len(s.Data)-1]
+	return top, nil
 }
 
 func (s *Stack) Print() {
-    if s == nil || len(s.Data) == 0 {
-        fmt.Println("stack is empty")
-        return
-    }
+	if s == nil || len(s.Data) == 0 {
+		fmt.Println("stack is empty")
+		return
+	}
 
-    for i:=len(s.Data)-1; i>=0; i-- {
-        fmt.Println(s.Data[i])
-    }
+	for i := len(s.Data) - 1; i >= 0; i-- {
+		fmt.Println(s.Data[i])
+	}
 }
