8129da8554a43678d674e981b754e05371aa7ca1
Santak Dalai <santakdalai90@live.com>
2024-07-27 14:01:51 +0000

    added spell checker

---
+++ test/dynamic_programming/spell_checker_test.go
@@ -0,0 +1,40 @@
+package dynamicprogramming
+
+import (
+	dynamicprogramming "golang-dsa/src/dynamic_programming"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckSpelling(t *testing.T) {
+	sc, err := dynamicprogramming.NewSpellChecker(
+		"../../src/dynamic_programming/sherlock_homes.txt",
+		1,
+		2,
+		5,
+	)
+
+	assert.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		word        string
+		corrections []string
+		err         error
+	}{
+		{"correct word", "information", []string{"information"}, nil},
+		{"incorrect word 1", "stroes", []string{"stores", "strokes"}, nil},
+		{"incorrect word 2", "primise", []string{"promise", "premise"}, nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			corrections, err := sc.CheckSpelling(test.word)
+			if test.err != nil {
+				assert.ErrorIs(t, test.err, err)
+			}
+			assert.Equal(t, test.corrections, corrections)
+		})
+	}
+}
